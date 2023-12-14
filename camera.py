c8437b395f8a0eda05595628afd34ffd69cbc66b
uzakotim <timauzakov@gmail.com>
2023-12-14 21:11:43 +0500

    working on raspberry pi

--- camera.py
+++ camera.py
@@ -1,9 +1,11 @@
 import cv2
-cap = cv2.VideoCapture(4)
+cap = cv2.VideoCapture(0)
+img = None
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    print("reading success")
+    #cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
 cap.release()
 cv2.destroyAllWindows()
